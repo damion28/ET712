81b447ed4e30935da16fe2d21e53b71593c6de76
damion28 <damion28.da@gmail.com>
2025-02-11 15:06:39 -0500

    Add files via upload

---
+++ lab 4/lab4_Ally.js
@@ -0,0 +1,35 @@
+/**
+ * Damion Ally
+ * Feb 11, function
+ */
+console.log("\n------------ Example 1: function -----------")
+// create a function to prompt 'Hello World'
+function msg(){
+    console.log("Hello World")
+}
+console.log("\n------------ Example 2: function to print 3, 2, 1 -----------")
+function printcount(){
+    for(let n=3; n>0; n--){
+        console.log(n)
+    }
+}
+console.log("\n------------ Example 3: function with parameter -----------")
+// pass a name into a function. The function will prompt the name in all uppercase
+function greeting(passname){
+    passname = passname.toUpperCase()
+    console.log(`Welcome to functions ${passname}`)
+}
+// function that takes two numbers and prompts the sum of them
+function sum(x,y){
+    console.log(`${x} + ${y} = ${x+y}`)
+}
+console.log("\n------------ Example 4: snake eyes -----------")
+// function to print 'snake eyes' if a 1 and 1 is passed to the function
+function snake_eyes(x,y){
+    if(x===1 && y===1){
+        console.log("SNAKE EYES")
+    }
+    else{
+        console.log("Not a snake eyes!")
+    }
+}
