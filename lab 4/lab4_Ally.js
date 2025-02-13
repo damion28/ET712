f89a8b6e8886ebce0ca9a7a09011de1b7eca71c0
damion28 <damion28.da@gmail.com>
2025-02-13 15:36:00 -0500

    lab4_Ally.js

--- lab 4/lab4_Ally.js
+++ lab 4/lab4_Ally.js
@@ -63,3 +63,55 @@ function checktemp(temperature){
 let t = 100
 let temp_result = checktemp(t)
 console.log(`Is the temperature greater than 75? ${temp_result}`)
+
+
+console.log("\n---------------- Example 7: built-in function --------------------")
+
+// built-in function in JS is called a global variable 
+
+// math function
+let num_p = Math.PI
+console.log(`JS PI value = ${num_p}`)
+
+// round() method returns the nearest ineger
+console.log(`rounded up PI = ${Math.round(num_p)}`)
+
+// ceil() method returns the round up of a number
+console.log(`ceil method = ${Math.ceil(num_p)}`)
+
+// pow() method returns the value of base to the power of exponent
+console.log(`2^3 = ${Math.pow(2,3)}`)
+
+// square root method --> sqrt()
+console.log(`square root of 9 = ${Math.sqrt(9)}`)
+
+// random number generator. random() method returns a random number between 0 and 1
+console.log(`random number = ${Math.random()}`)
+
+// random number between 1 and 9
+let n = 1 + Math.round(Math.random()*8)
+console.log(`number between 1 and 9 = ${n}`)
+
+
+console.log("\n---------------- Example 8: random number --------------------")
+
+// function to randomly generate a number between -5 and 5
+function randnumber() {
+    let p = -5 + Math.random(Math.random() * 10) // we put 10 because 10 + (-5) is equal to 5.
+    return p;
+}
+
+
+console.log("\n---------- Example 9: random number in a list (array) ----------")
+
+// function to randomly pick a color from a list of colors
+
+let colors = ["red", "green", "blue", "megenta", "gray"]
+function randomcolor(picked_index){
+    let list_size = colors.length       // find the length of the list
+    let last_index = list_size - 1      // find the last index of the list
+    let random_index = Math.random(Math.random() * last_index)      // randomly pick a number between 0 and the last index
+    return colors[random_index]     // return the random picked color
+}
+
+console.log(`color picked = ${randomcolor()}`)
