161813dd6e7f09f5883e5fa65693b04c68a87a1d
damion ally <damion.ally92@student.qcc.cuny.edu>
2025-03-04 14:52:12 -0500

    updating files

---
+++ java/lab 7/lab7_Ally.js
@@ -0,0 +1,7 @@
+console.log("Damion Ally")
+// select element by class name "description"
+let desc = document.querySelector(".description")
+console.log(desc)
+// select element by id name "title"
+let titleNode = document.querySelector("#title")
+console.log(titleNode)
