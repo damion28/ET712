06a237482c06498f963b2b761bbf3678767b9aba
damion ally <damion.ally92@student.qcc.cuny.edu>
2025-03-04 15:05:00 -0500

    updating file

--- java/lab 7/lab7_Ally.js
+++ java/lab 7/lab7_Ally.js
@@ -1,7 +1,19 @@
 console.log("Damion Ally")
+console.log("\n---- examlpe 1 ----")
 // select element by class name "description"
 let desc = document.querySelector(".description")
 console.log(desc)
 // select element by id name "title"
 let titleNode = document.querySelector("#title")
 console.log(titleNode)
+// select element/s by tag name, "p"
+let par = document.querySelectorAll("p")
+console.log(par)
+// select all elements with class name "methods"
+let methods = document.querySelectorAll(".methods")
+console.log(methods)
+// loop through each element in a node list
+console.log("\n---- example 2 loop through each element in a list list ----")
+for(let n = 0; n<methods.length; n++){
+    console.log(methods[n])
+}
