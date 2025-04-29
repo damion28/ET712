bdd2c7fe756a8131bea47382c7aa7dfa13bd6277
damion28 <damion28.da@gmail.com>
2025-04-29 23:23:35 +0000

    complete lab14

--- lab14/lab14/src/App.js
+++ lab14/lab14/src/App.js
@@ -10,19 +10,34 @@ function App() {
   // set state for multiple inputs. destructing useState
   // usestate({}) --> set a list of states
   const[input, setInputs] = useState({})
-  
-// function to open an alert dialog when form is submitted 
-const submitform = function(event){
-  event.preventDefault()
-  alert(`Welcome to React State ${setInputs.username}. Your lucky number is ${setInputs.luckynumber}`)
 
-}
+  // function to open an alert dialog when form is submitted 
+  const submitform = function(event){
+    event.preventDefault()
+    alert(`Welcome to React State ${input.username}. Your lucky number is ${input.luckynumber} \nComments = ${textcomment}`)
+  }
 
-  // function to handle the changes in all inputs
+  // function to handle the changes in multiple inputs
   const handle_changes = function(event){
     const name = event.target.name
     const value = event.target.value
+    setInputs(values => ({...values, [name]:value})) // spread statement
   }
+
+  // set the state for textarea
+  const[textcomment, setTextcomment] = useState("")
+  // function to collect the value of the textarea
+  const submitted_comments = function(event){
+    setTextcomment(event.target.value)
+  }
+
+  // set the state for select (multiple choice)
+  const[mygender, selectedGender] = useState("other")
+  // function to collect the gender
+  const collectgender = function(event){
+    selectedGender(event.target.value)
+  }
+
   return (
     <>
       <h1>Lab 14, React state</h1>
@@ -31,23 +46,56 @@ const submitform = function(event){
       <button onClick={()=>setCount(0)}>Reset Button</button>
 
       <h1>Form application using state</h1>
-      <form>
+      <form onSubmit={submitform}>
         <fieldset>
           <legend>Forms in ReactJS</legend>
-          <label for="username">Enter your name: </label>
-          <input
-            type='text'
-            id='username'
-            name = 'username'
-            value = {inputs.username}
-            onChange={(e)=>setName(e.target.value)}
+          <section>
+          <label htmlFor="username">Enter your name: </label>
+            <input
+              type='text'
+              id='username'
+              name = 'username'
+              value = {input.username}
+              onChange={handle_changes}
+              />
+          </section>
+          <section>
+            <label htmlFor="luckynumber">Enter a lucky number: </label>
+            <input
+              type='number'
+              id='luckynumber'
+              name='luckynumber'
+              value={input.luckynumber}
+              onChange={handle_changes}
             />
-            {/**submit form */}
+          </section>
+
+          <section>
+            <label for="comments">Any suggestions? </label>
+            <textarea
+              id='comments'
+              value={textcomment}
+              onChange={submitted_comments}
+            />
+          </section>
+
+          <section>
+            <select value={mygender} onChange={collectgender}>
+              <option value="female">Female</option>
+              <option value="male">Male</option>
+              <option value="others">Others</option>
+            </select>
+          </section>
+
+          {/**submit form */}
             <input type='submit'/>
         </fieldset>
       </form>
       {/**test data in the form after submission */}
-      <p>Name = {name}</p>
+      <p>Name = {input.username}</p>
+      <p>Lucky Number = {input.luckynumber}</p>
+      <p>Comments = {textcomment}</p>
+      <p>Gender = {mygender}</p>
     </>
   );
 }
